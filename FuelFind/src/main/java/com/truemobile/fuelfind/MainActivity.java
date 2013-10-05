6a0d7eb20d146a2235cdbc476eb23e446f4bfa3d
Werner <wernercscheffer@gmail.com>
2013-10-05 10:58:39 +0200

    Got MapView to work but is it API V1 and no longer used

--- FuelFind/src/main/java/com/truemobile/fuelfind/MainActivity.java
+++ FuelFind/src/main/java/com/truemobile/fuelfind/MainActivity.java
@@ -1,17 +1,42 @@
-package com\truemobile\fuelfind;
+package com.truemobile.fuelfind;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends MapActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
 
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ FuelFind/src/main/java/com/truemobile/fuelfind/MainMapFragment.java
@@ -0,0 +1,9 @@
+package com.truemobile.fuelfind;
+
+import com.google.android.gms.maps.MapFragment;
+
+/**
+ * Created by Werner on 2013/10/05.
+ */
+public class MainMapFragment extends MapFragment {
+}
